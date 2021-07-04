c440e4bcc0d6b8abd80145e4cd40f6c9f717d4dc
gulyasfeccferenc <gulyasfeccferenc@gmail.com>
2021-07-04 19:11:15 +0200

    Added logo (adaptive-icon.png), the main screens and basics of routing

---
+++ screens/Login.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+export default function Login() {
+    return (<p>Login screen works!</p>)
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
